30d0ae68a99cbcbf80ff630d407cdff7f6abe95d
Rodrigo Chaves <119129725+rmchaves04@users.noreply.github.com>
2024-03-18 14:36:28 -0300

    [feat] Calculate and display rolling returns

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import matplotlib.pyplot as plt
-import matplotlib.ticker as mtick
+from rolling import *
 import pandas as pd
 
-data = pd.read_csv('Data/data.csv', index_col='Date', date_format='%d/%m/%Y', sep=';')
+data = pd.read_csv('Data/FGS.csv', index_col='Date', date_format='%d/%m/%Y', sep=';')
 
 cagr = (data['Close'].iloc[-1] / data['Close'].iloc[0]) ** (1 / (len(data) / 252)) - 1
 
@@ -12,3 +11,6 @@ max_dd = daily_drawdown.min()
 
 print(f"CAGR: {cagr:.2%}")
 print(f"Max Drawdown: {max_dd:.2%}\n")
+
+returns = calculate_rolling_returns(data)
+plot_rolling_returns(returns)
---
+++ rolling.py
@@ -0,0 +1,40 @@
+import matplotlib.pyplot as plt
+import matplotlib.ticker as mtick
+
+def calculate_rolling_returns(data):
+    time_frames = [252, 504, 756, 1260]
+    results = {}
+
+    for tf in time_frames:
+        rolling_returns = data['Close'].pct_change(tf).rolling(tf).mean() * 100
+        min_return = rolling_returns.min()
+        max_return = rolling_returns.max()
+        avg_return = rolling_returns.mean()
+
+        results[tf//252] = {"Min": min_return, "Max": max_return, "Avg": avg_return}
+
+    return results
+
+def plot_rolling_returns(rolling_returns):
+    plt.figure(figsize=(12,6))
+
+    i = 0
+    for r in rolling_returns.values():
+        plt.vlines(i, r['Min'], r['Max'], colors='k', linestyles='solid')
+        plt.plot(i, r['Max'], 'g^', label='Maximum Return' if i == 0 else "")
+        plt.plot(i, r['Avg'], 's', color='gray', label='Average Return' if i == 0 else "")
+        plt.plot(i, r['Min'], 'rs', label='Minimum Return' if i == 0 else "")
+        i += 1
+
+    plt.xticks(range(len(rolling_returns)), [f'{k} years' for k in rolling_returns.keys()])
+    plt.title('Rolling Returns')
+    plt.xlabel('Time Frame')
+    plt.ylabel('Return')
+    plt.grid(True)
+
+    fmt = '%.0f%%'
+    yticks = mtick.FormatStrFormatter(fmt)
+    plt.gca().yaxis.set_major_formatter(yticks)
+
+    plt.legend()
+    plt.show()
