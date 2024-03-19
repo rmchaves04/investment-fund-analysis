e5a2ea852ed291b8080755ca4091f9cf8933f77f
Rodrigo Chaves <119129725+rmchaves04@users.noreply.github.com>
2024-03-19 11:11:15 -0300

    [feat] Plot volatility

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from rolling import *
+from vol import *
 import pandas as pd
 import numpy as np
 
@@ -11,14 +12,13 @@ rolling_max = data['Value'].cummax()
 daily_drawdown = data['Value'] / rolling_max - 1
 max_dd = daily_drawdown.min()
 
-data_monthly = data['Value'].resample('M').last()
-monthly_volatility = data_monthly.pct_change().std()
-annualized_monthly_volatility = monthly_volatility * np.sqrt(12)
+annualized_monthly_volatility = data['Value'].resample('M').last().pct_change().std() * np.sqrt(12)
 
 print(f"CAGR: {cagr:.2%}")
 print(f"Max Drawdown: {max_dd:.2%}")
 print(f"Anualized Monthly Volatility: {annualized_monthly_volatility:.2%}")
 
+plot_annualized_monthly_vol(data) 
 returns = calculate_daily_rolling_returns(data)
 plot_vertical_rolling_returns(returns)
 plot_monthly_rolling_returns(data, 6, benchmark)
---
+++ vol.py
@@ -0,0 +1,19 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.ticker as mtick
+
+def plot_annualized_monthly_vol(data):
+    data['Monthly Volatility'] = data['Value'].pct_change().rolling(20).std() * np.sqrt(252) * 100
+
+    plt.figure(figsize=(12,6))
+    plt.plot(data['Monthly Volatility'], label='Volatility', color='blue')
+    plt.title('Volatility')
+    plt.xlabel('Date')
+    plt.ylabel('Vol')
+    plt.grid(True)
+
+    yticks = mtick.FormatStrFormatter('%.2f%%')
+    plt.gca().yaxis.set_major_formatter(yticks)
+
+    plt.legend()
+    plt.show()
